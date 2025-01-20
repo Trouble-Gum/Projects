54aaebebced64cf23faa84118a91ea2c2d5249b4
Trouble-Gum <123637375+Trouble-Gum@users.noreply.github.com>
2025-01-20 14:28:21 +0300

    Delete SocialNetworkAnalyzer directory

--- SocialNetworkAnalyzer/SNA_unload.bat
+++
@@ -1,4 +0,0 @@
-C:\Projects\SocialNetworkAnalyzer\venv\Scripts\python.exe C:\Projects\SocialNetworkAnalyzer\main.py
-pause
-C:\Projects\yadisk\venv\Scripts\python.exe C:\Projects\yadisk\main.py
-pause
